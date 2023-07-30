1e60200b149bcdd24ff95c3795c4ec935c2a872e
TUAN2003 <tuananhvund0321@gmail.com>
2023-07-30 11:30:17 +0700

    change animation button send

--- app/src/main/java/com/example/chat_app/models/User.java
+++ app/src/main/java/com/example/chat_app/models/User.java
@@ -5,6 +5,4 @@ import java.io.Serializable;
 public class User implements Serializable {
     public String id, name, image, email, token,numberPhone,password;
     public String[] listIdFriend;
-
-    public User(){}
 }
