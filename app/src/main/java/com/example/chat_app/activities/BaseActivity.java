002e78b463ff8d8b81a31a544adf6e708cb1211e
TUAN2003 <tuananhvund0321@gmail.com>
2023-08-07 00:53:54 +0700

    change animation button send

--- app/src/main/java/com/example/chat_app/activities/BaseActivity.java
+++ app/src/main/java/com/example/chat_app/activities/BaseActivity.java
@@ -1,29 +1,39 @@
 package com.example.chat_app.activities;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.chat_app.service.AppCloseService;
+import com.example.chat_app.broadcast.MyReceive;
 import com.example.chat_app.utilities.Constants;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public abstract class BaseActivity extends AppCompatActivity {
     private DocumentReference documentReference;
+    private MyReceive myReceive;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FirebaseFirestore database = FirebaseFirestore.getInstance();
         documentReference = database.collection(Constants.KEY_COLLECTION_USERS)
                 .document(SignInActivity.preferenceManager.getString(Constants.KEY_USER_ID));
-        //startService(new Intent(this, AppCloseService.class));
+        registerReceiver(myReceive = new MyReceive(), new IntentFilter(Intent.ACTION_CLOSE_SYSTEM_DIALOGS));
     }
+
     @Override
     protected void onResume() {
         super.onResume();
         documentReference.update(Constants.KEY_STATUS, Boolean.TRUE);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(myReceive);
     }
 }
--- app/src/main/java/com/example/chat_app/service/AppCloseService.java
+++ app/src/main/java/com/example/chat_app/service/AppCloseService.java
@@ -3,35 +3,20 @@ package com.example.chat_app.service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
-import com.example.chat_app.activities.SignInActivity;
-import com.example.chat_app.utilities.Constants;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AppCloseService extends Service {
-    private FirebaseFirestore database;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        database = FirebaseFirestore.getInstance();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         return START_NOT_STICKY;
-    }
-
-    @Override
-    public void onTaskRemoved(Intent rootIntent) {
-        Toast.makeText(getApplicationContext(),"onTaskRemoved",Toast.LENGTH_SHORT).show();
-        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USERS)
-                .document(SignInActivity.preferenceManager.getString(Constants.KEY_USER_ID));
-        documentReference.update(Constants.KEY_STATUS,null);
     }
 
     @Nullable
