d186e792e930a870bdde92b55580b0120940c773
TUAN2003 <tuananhvund0321@gmail.com>
2023-08-07 22:51:15 +0700

    change animation button send

--- app/src/main/java/com/example/chat_app/fragments/HomeFragment.java
+++ app/src/main/java/com/example/chat_app/fragments/HomeFragment.java
@@ -108,7 +108,7 @@ public class HomeFragment extends Fragment implements ConversationListener {
                     conversations.add(conversation);
                     database.collection(Constants.KEY_COLLECTION_USERS)
                             .document(conversation.receiverId)
-                            .addSnapshotListener(parentActivity,(value1, error1) -> {
+                            .addSnapshotListener((value1, error1) -> {
                                 if(error1 != null)
                                     return;
                                 if(value1 != null){
