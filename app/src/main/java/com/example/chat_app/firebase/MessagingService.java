6ad2bf768a5c6b533dbaf82c44ac6e5f12777150
TUAN2003 <tuananhvund0321@gmail.com>
2023-08-21 15:03:30 +0700

    change animation button send

--- app/src/main/java/com/example/chat_app/firebase/MessagingService.java
+++ app/src/main/java/com/example/chat_app/firebase/MessagingService.java
@@ -1,9 +1,19 @@
 package com.example.chat_app.firebase;
 
-import androidx.annotation.NonNull;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.example.chat_app.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Objects;
+import java.util.Random;
 
 public class MessagingService extends FirebaseMessagingService {
     @Override
@@ -14,5 +24,25 @@ public class MessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        String title = Objects.requireNonNull(remoteMessage.getNotification()).getTitle();
+        String body = remoteMessage.getNotification().getBody();
+        showNotification(this,title,body);
+    }
+
+    private void showNotification(Context context, String title, String body) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"id");
+        builder.setSmallIcon(R.drawable.ic_notification);
+        builder.setContentTitle(title);
+        builder.setContentText(body);
+        builder.setPriority(NotificationCompat.PRIORITY_MAX);
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            String chanelId = "id1";
+            NotificationChannel notificationChannel = new NotificationChannel(chanelId,"chanel",NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(notificationChannel);
+            builder.setChannelId(chanelId);
+        }
+        notificationManager.notify(new Random().nextInt(),builder.build());
     }
 }
